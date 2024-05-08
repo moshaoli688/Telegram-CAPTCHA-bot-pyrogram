3a891ae425924cd36316b7c830c2d471f0545c9d
NimaQu <i@nimaqu.com>
2024-05-08 02:46:57 -0500

    set default count 1

--- db.py
+++ db.py
@@ -63,6 +63,8 @@ def _import_from_csv(file_path, table):
                 for row in reader:
                     if row[2] == "0":
                         continue
+                    if row[3] == "0":
+                        row[3] = "1"
                     linux_timestamp = int(row[1])
                     # 将时间戳转换为datetime对象
                     last_attempt = datetime.fromtimestamp(linux_timestamp)
